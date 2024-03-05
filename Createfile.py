9a4e5cc06f03152e7d2931a54de7acad1a506cfd
myleslipford <myles.lipford12@gmail.com>
2024-03-05 13:58:40 -0600

    Connected too database and creaed tables for different categories

---
+++ Createfile.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+# Connect to the SQLite database (or create it if not exists)
+conn = sqlite3.connect('databasefile1.db')
+cursor = conn.cursor()
+
+# Create tables for different categories
+categories = [
+    'Business Statistics',
+    'Business Database mgmt Science',
+    'General Biology II',
+    'Mgmt organization Behavior',
+    'Business Applications'
+]

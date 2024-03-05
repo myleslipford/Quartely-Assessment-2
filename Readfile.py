59302c0f44c703da0ee7aa96dc8e0f4e17415306
myleslipford <myles.lipford12@gmail.com>
2024-03-05 14:10:30 -0600

    Wrote a function called get all data which retrives all data from a table

--- Readfile.py
+++ Readfile.py
@@ -14,3 +14,17 @@ def get_table_names(conn):
   table_names = [row[0] for row in c.fetchall()]
   return table_names
 
+def get_all_data(conn, table_name):
+  """Retrieves all data (questions and answers) from a specific table.
+
+  Args:
+      conn: A database connection object.
+      table_name: The name of the table to retrieve data from.
+
+  Returns:
+      A list of dictionaries, where each dictionary contains a question and answer.
+  """
+  c = conn.cursor()
+  c.execute(f"SELECT * FROM {table_name}")
+  data = c.fetchall()
+  return [{'question_text': row[1], 'answer_text': row[2]} for row in data]
